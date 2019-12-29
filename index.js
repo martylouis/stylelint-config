9c2996f1733c408684635e9c23426fd2785b7c60
Marty Thierry <marty@martylouis.com>
2019-12-28 21:28:01 -0500

    Update stylelintrc config

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-module.exports = {
-  "extends": [
-    "stylelint-config-prettier",
-    "stylelint-config-idiomatic-order",
-    "stylelint-config-recommended-scss"
-  ],
-  "plugins": ["stylelint-scss"],
-  "defaultSeverity": "warning",
-  "rules": {
-    "at-rule-no-unknown": null,
-    "block-no-empty": null,
-    "declaration-colon-space-after": "always",
-    "declaration-empty-line-before": null,
-    "no-descending-specificity": null,
-    "number-leading-zero": "always",
-    "property-no-vendor-prefix": true,
-    "scss/at-rule-no-unknown": true,
-    "selector-combinator-space-after": "always",
-    "selector-no-qualifying-type": true,
-    "selector-no-vendor-prefix": true
-  }
-};
+const stylelintrc = require('./.stylelintrc');
+
+module.exports = stylelintrc;
